38338a97c76b219a6d2c59ee420b625dc8d1de33
Akhil Pillay <pillayakhil2@gmail.com>
2026-05-14 22:58:10 +0200

    Working V2: simplified app without lucide

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext'
 import './styles/globals.css'
-
-function SimpleApp() {
-  return (
-    <div style={{ padding: 40, textAlign: 'center' }}>
-      <h1>CSV Price Updater V2</h1>
-      <p>Password: Password123!</p>
-      <p>If you can see this, React is working!</p>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleApp />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
